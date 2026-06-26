a430402d17733700cd154288432f34a8f79a7fbd
akane9506 <akane19950611@gmail.com>
2026-06-26 13:45:24 -0700

    update queries, prepare for the db update implementation

--- db/queries/shopping_list.sql
+++ db/queries/shopping_list.sql
@@ -1,14 +1,18 @@
 -- name: CreateShoppingListItems :many
 WITH input_items AS (
     SELECT
-        btrim(item_name) AS name,
-        ord
-    FROM unnest(sqlc.arg(item_names)::text[]) WITH ORDINALITY AS t(item_name, ord)
-    WHERE length(btrim(item_name)) > 0
+        btrim(n.item_name) AS name,
+        c.category::shopping_category AS category,
+        n.ord
+    FROM unnest(sqlc.arg(item_names)::text[]) WITH ORDINALITY AS n(item_name, ord)
+    JOIN unnest(sqlc.arg(categories)::text[]) WITH ORDINALITY AS c(category, ord)
+        USING (ord)
+    WHERE length(btrim(n.item_name)) > 0
 ),
 deduped_items AS (
     SELECT DISTINCT ON (lower(name))
-        name
+        name,
+        category
     FROM input_items
     ORDER BY lower(name), ord
 )
@@ -20,7 +24,7 @@ INSERT INTO shopping_list (
 SELECT
     sqlc.arg(telegram_user_id),
     name,
-    sqlc.arg(category)::shopping_category
+    category
 FROM deduped_items
 ON CONFLICT (
     telegram_user_id,
@@ -34,5 +38,49 @@ RETURNING *;
 -- name: ListShoppingListItems :many
 SELECT *
 FROM shopping_list
-WHERE telegram_user_id = $1
+WHERE telegram_user_id = sqlc.arg(telegram_user_id)
 ORDER BY category, name;
+
+-- name: RemoveShoppingListItems :many
+WITH input_ids AS (
+    SELECT DISTINCT item_id
+    FROM unnest(sqlc.arg(item_ids)::bigint[]) AS t(item_id)
+)
+DELETE FROM shopping_list
+USING input_ids
+WHERE shopping_list.telegram_user_id = sqlc.arg(telegram_user_id)
+    AND shopping_list.id = input_ids.item_id
+RETURNING shopping_list.*;
+
+-- name: UpdateShoppingListItems :many
+WITH input_items AS (
+    SELECT
+        i.item_id,
+        btrim(n.item_name) AS name,
+        c.category::shopping_category AS category,
+        i.ord
+    FROM unnest(sqlc.arg(item_ids)::bigint[]) WITH ORDINALITY AS i(item_id, ord)
+    JOIN unnest(sqlc.arg(item_names)::text[]) WITH ORDINALITY AS n(item_name, ord)
+        USING (ord)
+    JOIN unnest(sqlc.arg(categories)::text[]) WITH ORDINALITY AS c(category, ord)
+        USING (ord)
+    WHERE i.item_id > 0
+        AND length(btrim(n.item_name)) > 0
+),
+deduped_items AS(
+    SELECT DISTINCT ON (item_id)
+        item_id,
+        name,
+        category
+    FROM input_items
+    ORDER BY item_id, ord
+)
+UPDATE shopping_list
+SET
+    name = deduped_items.name,
+    category = deduped_items.category,
+    updated_at = NOW()
+FROM deduped_items
+WHERE shopping_list.telegram_user_id = sqlc.arg(telegram_user_id)
+    AND shopping_list.id = deduped_items.item_id
+RETURNING shopping_list.*;
